b65911078396393b6853a4528e96f0a36cec6a82
eit <cheneit@gmail.com>
2022-05-04 15:53:35 +0800

    updata model

--- model/model.go
+++ model/model.go
@@ -5,8 +5,15 @@ type User struct {
 }
 
 type Profile struct {
-	ID   int64 `gorm:"primaryKey"`
-	User User  `gorm:"foreignKey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	// UserID int64 `gorm:"column:user_id"`
-	// User   User  `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID     int64  `gorm:"primaryKey"`
+	Name0  string `gorm:"column:name_0;not null;uniqueIndex:name_idx"`
+	Name1  string `gorm:"column:name_1;not null;unique:name_idx"`
+	Name2  string `gorm:"column:name_2;not null;idx:name_idx"`
+	Name3  string `gorm:"column:name_3;not null;idx:name_idx;unique"`
+	Name4  string `gorm:"column:name_4;not null;index:name_idx,unique"`
+	Name5  string `gorm:"column:name_5;not null;index:name_idx;unique"`
+	Name6  string `gorm:"column:name_6;not null;index:name_idx;uniqueIndex"`
+	Name7  string `gorm:"column:name_7;not null;uniqueIndex"`
+	UserID int64  `gorm:"column:user_id"`
+	User   User   `gorm:"foreignKey:UserID"`
 }
